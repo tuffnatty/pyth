d369a3601f46d9744749811db6190625febe1d2c
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2015-07-11 17:38:08 +0200

    examples/reading/rtf15.py can now generate reference test output

--- examples/reading/rtf15.py
+++ examples/reading/rtf15.py
@@ -4,16 +4,22 @@ import sys
 import os.path
 
 from pyth.plugins.rtf15.reader import Rtf15Reader
-from pyth.plugins.xhtml.writer import XHTMLWriter
+from pyth.plugins.xhtml.writer import XHTMLWriter, write_html_file
 
+numargs = len(sys.argv) - 1
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
+if numargs not in [1, 2]:
+    print("usage: rtf15 inputfile.rtf [outputdir]")
 else:
-    filename = os.path.normpath(os.path.join(
-        os.path.dirname(__file__), 
-        '../../tests/rtfs/sample.rtf'))
-
-doc = Rtf15Reader.read(open(filename, "rb"))
-
-print(XHTMLWriter.write(doc, pretty=True).read())
+    inputfile = sys.argv[1]
+    doc = Rtf15Reader.read(open(inputfile, "rb"))
+    the_output = XHTMLWriter.write(doc, pretty=True).read()
+    if numargs == 1:
+        print("<!-- ##### RTF file" + inputfile + "as XHTML: -->")
+        print(the_output)
+    else:
+        basename = os.path.basename(inputfile)
+        outputdir = sys.argv[2]
+        outputfile = os.path.join(outputdir,
+                                  os.path.splitext(basename)[0] + ".html")
+        write_html_file(outputfile, the_output, print_msg=True)
