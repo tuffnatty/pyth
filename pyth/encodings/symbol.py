1cd7d46d48c5956d39c9debc7c2fccecdb26714b
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2015-07-18 20:38:51 +0200

    encodings/symbol.py: py3-migrated and improved
    - performed changes needed to run under Python 3
    - fixed one mistake in decodeTable
    - added blank to the codes list
    - improved the error message
    - added two more test cases:
      - msword-symbol.rtf
      - wordpad-symbol.rtf
    - neither of them works correctly, see 'Limitations' section
      in top-level README
    - therefore, the corresponding 'correct' outputs
      are empty files (to make the automated tests fail)

--- pyth/encodings/symbol.py
+++ pyth/encodings/symbol.py
@@ -8,12 +8,13 @@ import six
 from six.moves import map
 
 decodeTable = {
+    32: 32,
     33: 33, 34: 8704, 35: 35, 36: 8707, 37: 37, 38: 38, 39: 8717, 40: 40, 41: 41, 42: 42, 43: 43, 44: 44, 45: 45, 46: 46,
     47: 47, 48: 48, 49: 49, 50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55, 56: 56, 57: 57, 58: 58, 59: 59, 60: 60, 61: 61,
     62: 62, 63: 63, 64: 8773, 65: 913, 66: 914, 67: 935, 68: 916, 69: 917, 70: 934, 71: 915, 72: 919, 73: 921, 74: 977,
     75: 922, 76: 923, 77: 924, 78: 925, 79: 927, 80: 928, 81: 920, 82: 929, 83: 931, 84: 932, 85: 933, 86: 962, 87: 937,
     88: 926, 89: 936, 90: 918, 91: 91, 92: 8756, 93: 93, 94: 8869, 95: 95, 96: 63717, 97: 945, 98: 946, 99: 967, 100: 948,
-    101: 949, 102: 966, 103: 947, 104: 951, 105: 953, 106: 981, 107: 954, 108: 955, 109: 956, 110: 957, 111: 959, 112: 960,
+    101: 949, 102: 981, 103: 947, 104: 951, 105: 953, 106: 966, 107: 954, 108: 955, 109: 956, 110: 957, 111: 959, 112: 960,
     113: 952, 114: 961, 115: 963, 116: 964, 117: 965, 118: 982, 119: 969, 120: 958, 121: 968, 122: 950, 123: 123, 124: 124,
     125: 125, 126: 126, 160: 8364, 161: 978, 162: 697, 163: 8804, 164: 8260, 165: 8734, 166: 402, 167: 9827, 168: 9830,
     169: 9829, 170: 9824, 171: 8596, 172: 8592, 173: 8593, 174: 8594, 175: 8595, 176: 176, 177: 177, 178: 698, 179: 8805,
@@ -27,19 +28,20 @@ decodeTable = {
 
 encodeTable = dict((v, k) for (k, v) in six.iteritems(decodeTable))
 
-ERROR_STRING = "Ordinal not in range (255)"
+ERROR_STRING = "Ordinal not in known range 32-254"
 
 def symbol_decode(input, errors='strict'):
     chars = []
-    for (i, c) in enumerate(input):
+    #for (i, c) in enumerate(input):
+    for i in range(len(input)):
         try:
-            chars.append(decodeTable[ord(c)])
+            chars.append(decodeTable[six.indexbytes(input, i)])
         except KeyError:
             if errors == 'replace':
                 chars.append(ord(u'?'))
             else:
                 raise UnicodeDecodeError("symbol", input, i, i+1, ERROR_STRING)
-    return (u"".join(map(unichr, chars)), len(input))
+    return (u"".join(map(six.unichr, chars)), len(input))
             
 
 def symbol_encode(input, errors='strict'):
